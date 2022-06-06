83ea6ff68d2dd50e73eab1d9cb00e1054730768d
gynchiii <thepatrinot@protonmail.com>
2022-06-06 07:20:54 -0300

    :rocket: Edit Project woring made mutation to edit the project applied condition on the save button, if the ID already exists it performs the commit which calls the mutation 'EDIT_PROJECT' on the same condition, if there is no ID it performs a commit that calls the 'ADD_PROJECTS' mutation

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,8 +19,12 @@ export const store = createStore<State>({
             name: projectName
         } as IProject
         state.projects.push(project)
+    },
+    'EDIT_PROJECT'(state, project: IProject) {
+      const index = state.projects.findIndex( proj => proj.id == project.id)
+      state.projects[index] = project
     }
-  }
+}
 })
 export function useStore(): Store<State> {
   return vuexUseStore(key)
